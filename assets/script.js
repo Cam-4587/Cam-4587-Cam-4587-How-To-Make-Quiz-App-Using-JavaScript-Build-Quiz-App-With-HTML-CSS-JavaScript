7ffceccb12a11aff856826f7576ba8fe802811aa
Cam-4587 Conroy <cameronconroy@live.com>
2024-05-21 14:42:24 +0000

    Javascript code added

---
+++ assets/script.js
@@ -0,0 +1,127 @@
+// the first question will have an index of 0, the second will have an index of 1, the third will have an index of 2 and so on
+const questions = [
+    {
+        question: "Which is largest animal in the world?"
+        answers:[
+            {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is smallest country in the world?"
+        answers: [
+            {text: "Vatican City", correct: true},
+            {text: "Bhutan", correct: false},
+            {text: "Nepal", correct: false},
+            {text: "Shri Lanka", correct: false},
+        ]
+    },
+    {
+        question: "Which is largest desert in the world?"
+        answers: [
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: false},
+            {text: "Sahara", correct: false},
+            {text: "Antarctica", correct: true},
+        ]
+    },
+    {
+        question: "Which is smallest continent in the world?"
+        answers: [
+            {text: "Asia", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Arctic", correct: false},
+            {text: "Africa", correct: false},  
+        ]  
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-button");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex =0;
+let score = 0;
+
+function startQuiz() {
+   let currentQuestionIndex = 0;
+    let score = 0;
+}
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+// if you refer to the description at the top in conjunction to this,
+// the currentQuestionIndex is the index mentioned at the top, so the currentQuesion is equal to the question index
+function showQuestion (){
+    let currentQuestion = question [currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + "." + currentQuestion.
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+        if(answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState() {
+    nextButton.style.display ="none";
+    while(answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+function selectAnswer(){
+    const selectBtn = e.target;
+    const isCorrect = selectBtn.dataset.correct === "true";
+    if(isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display="block";
+    }
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButtonstyle.display = "block";
+    nextButton.style.display = "block";
+}
+    function handleNextButton(){
+        currentQuestionIndex++;
+        if(currentQuestionIndex < questions.length) {
+            showQuestion();
+        }else {
+            showScore();
+        }
+    }
+    nextButton.addEventListener("click", (=>){
+        if(currentQuestionIndex < questions.length) {
+            handleNextButton();
+        } else{
+            startQuiz();
+        }
+        });
+    
+
+startQuiz (){
+}
